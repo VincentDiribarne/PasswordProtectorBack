514165e28bcc01e216482f57fd61a646df27203d
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2024-04-05 09:35:19 +0200

    Start commit

---
+++ src/main/java/org/ahv/passwordprotectorback/PasswordProtectorBackApplication.java
@@ -0,0 +1,13 @@
+package org.ahv.passwordprotectorback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordProtectorBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordProtectorBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ahv/passwordprotectorback/PasswordProtectorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahv.passwordprotectorback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordProtectorBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
